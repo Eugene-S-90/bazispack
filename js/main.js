041ff20dd9acc482d2106cd35f9ee564ce820a73
Eugene <slon4ig@gmail.com>
2017-04-18 17:20:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,227 @@
+/*Смена фона при наведении 1-я страница */
+$(document).ready(function(){
+$("#production").mouseenter(function(){
+    $("body").css( {
+'background':'url(img/a4.jpg) no-repeat center center',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s all',
+'-moz-transition':'1.0s all',
+'-webkit-transition':'1.0s all',
+'-o-transition':'1.0s all',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#about-company").mouseenter(function(){
+$("body").css( {
+'background':'url(img/a2.jpg) no-repeat center center',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s all',
+'-moz-transition':'1.0s all',
+'-webkit-transition':'1.0s all',
+'-o-transition':'1.0s all',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#contacts").mouseenter(function(){
+$("body").css( {
+'background':'url(img/contact.jpg) no-repeat center center',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s all',
+'-moz-transition':'1.0s all',
+'-webkit-transition':'1.0s all',
+'-o-transition':'1.0s all',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+/*Смена фона при наведении  раздела ПРОДУКЦИЯ */
+
+$(document).ready(function(){
+$("#home").mouseenter(function(){
+$("body").css( {
+'background':'url(img/a1.jpeg) no-repeat center center',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s ease-in',
+'-moz-transition':'1.0s ease-in',
+'-webkit-transition':'1.0s ease-in',
+'-o-transition':'1.0s ease-in',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#product1").mouseenter(function(){
+$("body").css( {
+'background':'url(img/termo.jpeg) no-repeat center center',
+'height':'auto',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s ease-in',
+'-moz-transition':'1.0s ease-in',
+'-webkit-transition':'1.0s ease-in',
+'-o-transition':'1.0s ease-in',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#product2").mouseenter(function(){
+$("body").css( {
+'background':'url(img/mnogo.jpg) no-repeat center center',
+'height':'auto',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s ease-in',
+'-moz-transition':'1.0s ease-in',
+'-webkit-transition':'1.0s ease-in',
+'-o-transition':'1.0s ease-in',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#product4").mouseenter(function(){
+$("body").css( {
+'background':'url(img/black.jpg) no-repeat center center',
+'height':'auto',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s ease-in',
+'-moz-transition':'1.0s ease-in',
+'-webkit-transition':'1.0s ease-in',
+'-o-transition':'1.0s ease-in',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+$(document).ready(function(){
+$("#product3").mouseenter(function(){
+$("body").css( {
+'background':'url(img/str.jpg) no-repeat center center',
+'height':'auto',
+'background-attachment': 'fixed',
+'background-size':'cover',
+'-moz-background-size':'cover',
+'-webkit-background-size':'cover',
+'-o-background-size':'cover',
+'transition':'1.0s ease-in',
+'-moz-transition':'1.0s ease-in',
+'-webkit-transition':'1.0s ease-in',
+'-o-transition':'1.0s ease-in',
+'transition-delay': '0.3s',
+'-moz-transition-delay': '0.3s',
+'-webkit-transition-delay': '0.3s',
+'-o-transition-delay': '0.3s',
+'opacity':'1.0',
+'-moz-opacity':'1.0',
+'-webkit-opacity':'1.0',
+'-o-opacity':'1.0',
+}
+)});
+});
+
+
+
+
+
+
+/*Загрузка меню продукция */
+function goProduction() {
+$("#ajax").load("production.html", function() {
+});
+};
+/*Загрузка меню ГЛАВНАЯ */
+function goHome() {
+$("#ajax").load("home.html", function() {
+});
+};
+
+function goAbout() {
+$("#ajax").load("about.html", function() {
+});
+};
